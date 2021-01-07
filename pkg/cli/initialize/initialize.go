dc73b0c55c9f8951f88000381609f05aa3decd32
viniciussanchez <viniciuss.sanchez@gmail.com>
2021-01-07 16:12:55 -0300

    Added init example and long message

--- pkg/cli/initialize/initialize.go
+++ pkg/cli/initialize/initialize.go
@@ -7,8 +7,10 @@ import (
 // NewCmdInitialize add the command line init
 func NewCmdInitialize() *cobra.Command {
 	return &cobra.Command{
-		Use:   "init",
-		Short: "Initialize a new project",
+		Use:     "init",
+		Short:   "Initialize a new project",
+		Long:    "Initialize a new project and creates a boss.json file",
+		Example: "  Initialize a new project:\n  boss init\n\n  Initialize a new project without having it ask any questions:\n  boss init -q",
 		Run: func(cmd *cobra.Command, args []string) {
 			initializeBoss()
 		},
